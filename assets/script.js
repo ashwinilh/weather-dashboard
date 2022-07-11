2167994bdd62c4827e55c845745cd10c9bf63ee1
ashwini lalithaHuchahanumegowda <ashwini.lh12@gmail>
2022-07-10 20:49:23 -0500

    updated minor cahnges

--- assets/script.js
+++ assets/script.js
@@ -9,6 +9,7 @@ var currentHumidty = $("#humidity");
 var currentWSpeed = $("#wind-speed");
 var currentUvindex = $("#uv-index");
 var sCity = [];
+
 // searches the city to see if it exists in the entries from the storage
 
 function find(c) {
@@ -31,20 +32,20 @@ function displayWeather(event) {
     }
 }
 
-// Here we create the AJAX call
+//  create the AJAX call
 function currentWeather(city) {
-    // Here we build the URL so we can get a data from server side.
+    //  build the URL to get a data from server side.
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
     $.ajax({
         url: queryURL,
         method: "GET",
     }).then(function (response) {
 
-        // parse the response to display the current weather including the City name. the Date and the weather icon. 
+        // parse the response to display the current weather including the City name, the Date and the weather icon. 
         console.log(response);
-        //Dta object from server side Api for icon property.
-        var weathericon = response.weather[0].icon;
-        var iconurl = "https://openweathermap.org/img/wn/" + weathericon + "@2x.png";
+        //Data object from server side Api for icon property.
+        var weatherIcon = response.weather[0].icon;
+        var iconurl = "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
         // The date format method is taken from the  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
         var date = new Date(response.dt * 1000).toLocaleDateString();
         //parse the response for name of city and concanatig the date and icon.
@@ -109,15 +110,15 @@ function forecast(lon, lat) {
 
         for (i = 0; i < 5; i++) {
             var date = new Date((response.daily[i].dt) * 1000).toLocaleDateString();
-            var iconcode = response.daily[i].weather[0].icon;
-            var iconurl = "https://openweathermap.org/img/wn/" + iconcode + ".png";
+            var iconCode = response.daily[i].weather[0].icon;
+            var iconUrl = "https://openweathermap.org/img/wn/" + iconCode + ".png";
             var tempF = response.daily[i].temp.max;
             var humidity = response.daily[i].humidity;
             var wind = response.daily[i].wind_speed + ' mph';
 
             $("#fDate" + i).html(date);
-            $("#fImg" + i).html("<img src=" + iconurl + ">");
-            $("#wind" + i).html(wind + "&#8457");
+            $("#fImg" + i).html("<img src=" + iconUrl + ">");
+            $("#wind" + i).html(wind);
             $("#fTemp" + i).html(tempF + "&#8457");
             $("#fHumidity" + i).html(humidity + "%");
         }
